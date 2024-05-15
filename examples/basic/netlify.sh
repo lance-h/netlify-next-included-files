e927c3ea374a87f67cede7f5578e242749d1164f
Lance Hambling <lance.h@apolinargroup.com>
2024-05-16 10:32:19 +1200

    WIP

--- examples/basic/netlify.sh
+++ examples/basic/netlify.sh
@@ -1,16 +1,19 @@
+# echo LOCALTEST=Foo >> .env.local
+# echo
+# echo "-----"
+# echo
+# pwd
+# cd ../..
+# pwd
+# npm run build:example
+# cd ./examples/basic
+# pwd
+# ls -a
+# cd .next
+# ls -a
+# echo
+# echo "-----"
+# echo
+
 echo LOCALTEST=Foo >> .env.local
-echo
-echo "-----"
-echo
-pwd
-cd ../..
-pwd
-npm run build:example
-cd ./examples/basic
-pwd
-ls -a
-cd .next
-ls -a
-echo
-echo "-----"
-echo
+npm run build
