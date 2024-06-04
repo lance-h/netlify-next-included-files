736768d606f74d4ddd0904c7b3989b2069786d4e
Lance Hambling <lance.h@apolinargroup.com>
2024-06-05 11:36:20 +1200

    WIP

--- examples/basic/lib/cache.ts
+++ examples/basic/lib/cache.ts
@@ -2,9 +2,16 @@ export type WithCache = {
     cache: { timestamp: number };
 };
 
-const processWithCache: WithCache = {
-    cache: { timestamp: 0 },
-};
+// Data not shared between ISR and API in local
+// const processWithCache: WithCache = {
+//     cache: { timestamp: 0 },
+// };
+// Data IS shared between ISR and API in local
+const processWithCache = process as unknown as WithCache;
+
+if (!processWithCache.cache) {
+  processWithCache.cache = { timestamp: 0 };
+}
 
 export default processWithCache.cache;
   
