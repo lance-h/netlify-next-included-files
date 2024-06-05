f99d2f5fe1dd2530b99495342c43524417b69593
Lance Hambling <lance.h@apolinargroup.com>
2024-06-06 11:46:07 +1200

    WIP

--- examples/basic/app/app-dynamic/[[...segment]]/page.tsx
+++ examples/basic/app/app-dynamic/[[...segment]]/page.tsx
@@ -1,36 +1,52 @@
 import cache from "@/lib/cache";
 import provider from "@/lib/pages"
 import Link from "next/link";
-import { notFound } from "next/navigation";
+import { notFound, redirect } from "next/navigation";
 
-export const revalidate = 3600;
+// export const revalidate = 3600;
 
-export default async function Page(props: any) {
-    // const url = ['', ...(props.params.segment || [''])].join('/');
-    const url = `/${(props.params.segment || []).join('/')}`;
-    // const url = `/${(props.params.segment).join('/') || ''}`;
+type Params = {
+    segment: Array<string>;
+}
+
+type Props = {
+    params: Params;
+}
+
+export default async function Page({ params }: Props) {
+    const url = `/${(params.segment || []).join('/')}`;
     console.log('Dynamic Page', { url, date: new Date(), cache });
-    const page = await provider.get(url).catch((err) => {
-        console.error('Error during page fetch', err);
-        return undefined;
-    });
+    // const page = await provider.get(url).catch((err) => {
+    //     console.error('Error during page fetch', err);
+    //     return undefined;
+    // });
+    const page = {
+        date: new Date()
+    };
 
-    if (!page) {
+    if (url.startsWith('/redirect/')) {
+        return redirect(`/redirected/${page.date.toISOString()}`)
+    }
+
+    // if (!page) {
+    //     return notFound();
+    // }
+    if (url.startsWith('/not-found/')) {
         return notFound();
     }
 
-    const pages = (await provider.list());
+    // const pages = (await provider.list());
 
     return <div>
         <h1>Using App Router</h1>
-        {/* NOTE: Do NOT render props directly - it appears that attempting to consume props.searchParams opts the request into SSR */}
-        <pre>{JSON.stringify(props.params)}</pre>
+        <div id="test">{page.date.toISOString()}</div>
+        <pre>{JSON.stringify(params)}</pre>
         <pre>{JSON.stringify(page)}</pre>
         <pre>{JSON.stringify(cache)}</pre>
-        <h2>All Pages</h2>
+        {/* <h2>All Pages</h2>
         <ul>
             {pages.map((page) => <li key={page}><Link prefetch={false} href={`/app-dynamic${page}`}>{page}</Link></li>)}
-        </ul>
+        </ul> */}
     </div>
 }
 
--- examples/basic/app/data/route.ts
+++ examples/basic/app/data/route.ts
@@ -1,12 +1,20 @@
 import provider from '@/lib/pages';
-import { revalidateTag } from 'next/cache';
+import { revalidatePath, revalidateTag } from 'next/cache';
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
     const { searchParams } = new URL(request.url);
     const action = searchParams.get('action');
     const url = searchParams.get('url');
-    if (action === 'revalidate') {
+    if (action === 'nuke') {
+        const tag = searchParams.get('tag') || ''
+        console.log('Revalidating', tag);
+        revalidatePath('/app-dynamic/[[...segment]]', 'page');
+    } else if (action === 'revalidatepath') {
+        const path = searchParams.get('path') || ''
+        console.log('Revalidating', path);
+        revalidatePath(path);
+    } else if (action === 'revalidate') {
         const tag = searchParams.get('tag') || ''
         console.log('Revalidating', tag);
         revalidateTag(tag);
