ffd2f98f8d5a309b2720634eda2db7a59f7e1a81
Lance Hambling <lance.h@apolinargroup.com>
2024-06-06 14:09:45 +1200

    ISR Issue

--- examples/basic/app/data/route.ts
+++ examples/basic/app/data/route.ts
@@ -1,37 +1,19 @@
-import provider from '@/lib/pages';
-import { revalidatePath, revalidateTag } from 'next/cache';
+import { revalidatePath } from 'next/cache';
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
     const { searchParams } = new URL(request.url);
     const action = searchParams.get('action');
-    const url = searchParams.get('url');
     if (action === 'nuke') {
-        const tag = searchParams.get('tag') || ''
-        console.log('Revalidating', tag);
-        revalidatePath('/app-dynamic/[[...segment]]', 'page');
+        console.log('Nuking');
+        revalidatePath('/[[...segment]]', 'page');
+        return NextResponse.json({ message: 'Nuked', timestamp: new Date() });
     } else if (action === 'revalidatepath') {
         const path = searchParams.get('path') || ''
         console.log('Revalidating', path);
         revalidatePath(path);
-    } else if (action === 'revalidate') {
-        const tag = searchParams.get('tag') || ''
-        console.log('Revalidating', tag);
-        revalidateTag(tag);
-    } else if (action === 'revalidateall') {
-        console.log('Revalidating all');
-        revalidateTag('blobs');
-        revalidateTag('contentfulall');
-    } else if (action === 'add' && typeof url === 'string') {
-        await provider.add(url, { title: searchParams.get('title') });
-        revalidateTag(`page:${url}`);
-    } else if (action === 'remove' && typeof url === 'string') {
-        await provider.remove(url);
-        revalidateTag(`page:${url}`);
-    } else if (action === 'view' && typeof url === 'string') {
-        const page = await provider.get(url)
-        return NextResponse.json({ data: page });
+        return NextResponse.json({ message: `Revalidated ${path}`, timestamp: new Date() });
     }
 
-    return NextResponse.json({ data: await provider.list() });
+    return new Response(undefined, { status: 400 });
 }
